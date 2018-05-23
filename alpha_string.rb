b0c87e69d12ca4f7cf65701bd6799210fda4931c
Pete Sword <pete.sword@yahoo.com>
2018-05-23 17:10:01 -0600

    working program

--- alpha_string.rb
+++ alpha_string.rb
@@ -3,11 +3,11 @@ def sorted_string
   puts "Enter a word to have the letters returned alphabetically: "
   word = gets.strip.downcase.gsub(" ", "")
   split_string = word.split("")
-  sorted_word = []
-  word.size.times {sorted_word << split_string.sort}
+  sorted_word = split_string.sort
   sorted_word.join
   puts "#{word} in alphabetical order is #{sorted_word.join}"
-    
+  keep_going
+  
 end
 
 def keep_going
@@ -18,10 +18,10 @@ def keep_going
   
   while continue == 1
     sorted_string
+  end
     if continue == 2
       exit
     end
-  end
 
 end
 
